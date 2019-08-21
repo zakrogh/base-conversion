e55281c609d7902cfea936bb70a6715acbec492d
Zach Krogh <zakrogh@gmail.com>
2019-08-21 15:30:42 -0700

    consolodate conversion functions into single method, converts base 10 into binary or trinary or hex

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 var output = []
+var getHexLetter = function (num){
+  switch(num){
+    case 10:
+      return "A";
+    case 11:
+      return "B";
+    case 12:
+      return "C";
+    case 13:
+      return "D";
+    case 14:
+      return "E";
+    case 15:
+      return "F";
+    default:
+      return num;
+  }
+}
 var toBinary = function(num, div){
   if(div < 1)
     return;
@@ -15,20 +33,53 @@ var toTrinary = function(num){
 
 }
 var toHexadecimal = function(num){
-  
+  if(num === 0){
+    return;
+  }else{
+    var remainder = num % 16;
+    if(num <= 16){
+      output.unshift(getHexLetter(num));
+      num = 0;
+    }else{
+      num = Math.floor(num / 16);
+      output.unshift(getHexLetter(remainder));
+    }
+
+    toHexadecimal(num);
+  }
+}
+
+var convertNum = function(num, base){
+  if(num === 0){
+    return;
+  }else{
+    var remainder = num % base;
+    if(num < base){
+      output.unshift(getHexLetter(num));
+      num = 0;
+    }else{
+      num = Math.floor(num / base);
+      output.unshift(getHexLetter(remainder));
+    }
+
+    convertNum(num, base);
+  }
 }
 
 var selectBase = function(num, base){
   switch(base){
     case "binary":
-      toBinary(num, Math.pow(2, 32));
-      trimZeroes();
+      //toBinary(num, Math.pow(2, 32));
+      //trimZeroes();
+      convertNum(num, 2);
     break;
     case "trinary":
-      toTrinary(num);
+      //toTrinary(num);
+      convertNum(num, 3);
     break;
     case "hexadecimal":
-      toHexadecimal(num);
+      //toHexadecimal(num);
+      convertNum(num, 16);
     break;
   }
 }
