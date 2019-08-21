7bca2bdbc76db779f8e96e6c044655deb8de33ba
Zach Krogh <zakrogh@gmail.com>
2019-08-21 13:54:37 -0700

    convert to binary but not trinary or hex yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-var toBinary = function(num){
+var output = []
+var toBinary = function(num, div){
+  if(div < 1)
+    return;
+  if((num / div) >= 1){
+    output.push(1);
+    toBinary(num - div, div/2);
+  }else {
+    output.push(0);
+    toBinary(num, div/2);
+  }
 
 }
 var toTrinary = function(num){
@@ -8,21 +18,34 @@ var toHexadecimal = function(num){
   
 }
 
+var selectBase = function(num, base){
+  switch(base){
+    case "binary":
+      toBinary(num, Math.pow(2, 32));
+      trimZeroes();
+    break;
+    case "trinary":
+      toTrinary(num);
+    break;
+    case "hexadecimal":
+      toHexadecimal(num);
+    break;
+  }
+}
+
+var trimZeroes = function(){
+  output = output.slice(output.indexOf(1));
+}
+
 $(document).ready(function(){
   $(".mainform").submit(function(event){
     var userInput = parseInt($("#userInput").val());
     var base = $(".base").val();
-    switch(base){
-      case "binary":
-      //do stuff
-      break;
-      case "trinary":
-      //do stuff
-      break;
-      case "hexadecimal":
-      //do stuff
-      break;
-    }
+    output = [];
+    selectBase(userInput, base);
+    $(".output").text("");
+    $(".output").append(output.join(""));
+    $(".output").fadeIn();
     event.preventDefault();
   });
 });
